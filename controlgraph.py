71426b64969bb72c734f9fbcd77a4742dbb3df15
Tyler Heucke <tyler.heucke@dell.com>
2018-08-30 11:35:26 -0700

    Process build deps faster
    * Use text parsing instead of `dpkg-checkbuilddeps`
    * Remove dbp/docker use and `--rm` flag
    
    * Process commands in parallel
    
    Signed-off-by: Tyler Heucke <tyler.heucke@dell.com>

--- controlgraph.py
+++ controlgraph.py
@@ -1,110 +1,85 @@
 """Generate build order graph from directory of debian packaging repositories"""
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
 
 import argparse
 import logging
 import re
-import shutil
 import sys
 
 from pathlib import Path
 from subprocess import Popen, run, PIPE, DEVNULL, STDOUT
 from typing import Dict, List, Tuple
 
-import dbp
 import networkx as nx
 
-L = logging.getLogger("dbp")
+from debian import deb822
+
+L = logging.getLogger("controlgraph")
 L.addHandler(logging.NullHandler())
 
 
-def docker_exec_command(cmd: List[str]) -> List[str]:
-    """Returns exit code, stdout, and stderr"""
-    return [
-        "docker",
-        "exec",
-        "-it",
-        "--user=build",
-        "-e=UID={}".format(dbp.UID),
-        "-e=GID={}".format(dbp.GID),
-        dbp.CONTAINER_NAME,
-        "bash",
-        "-l",
-        "-c",
-        " ".join(cmd),
-    ]
+class BinaryPackage:
+    def __init__(self, name: str, source: str, build_deps: List[str]) -> None:
+        self.name = name
+        self.source = source
+        self.build_deps = build_deps
+
+    def __repr__(self) -> str:
+        return "BinaryPackage({})".format(self.__dict__)
 
 
-def get_source_map(dirs: List[Path]) -> Dict[str, str]:
-    """Returns a map of binary packages to source packages found in `dirs`"""
-    deps = {}
+def parse_controlfile(path: Path) -> Dict[str, BinaryPackage]:
+    source = ""
+    build_deps = []
+    pkgs = {}
+    with path.open() as f:
+        for src in deb822.Sources.iter_paragraphs(f):
+            if "Source" in src:
+                source = src["Source"]
+                build_deps.extend(
+                    [
+                        re.sub(r" \([^\)]*\)", "", s.strip()).strip()
+                        for s in src["Build-Depends"].split(",")
+                    ]
+                )
+            elif "Package" in src:
+                pkgs[src["Package"]] = BinaryPackage(src["Package"], source, build_deps)
+    return pkgs
 
+
+def graph(dirs: List[Path]) -> nx.DiGraph:
+    """Prints linear or dot graph build order."""
+    # Get dict matching binary packages to source packages/build deps
+    pkgs = {}
     for p in dirs:
-        control = Path(p / "debian/control").read_text()
-        binaries = []
-        for line in control.split("\n"):
-            if line.startswith("Package: "):
-                deps[line[len("Package: ") :]] = p.stem
+        controlfile = Path(p / "debian/control")
+        if not controlfile.exists():
+            continue
+        pkgs.update(parse_controlfile(controlfile))
 
-    return deps
+    # Get dict matching source package to build deps
+    build_deps = {v.source: v.build_deps for _, v in pkgs.items()}
+    # Get dict matching binary package to source package
+    src_pkgs = {v.name: v.source for _, v in pkgs.items()}
 
+    # Convert binary build deps to source packages (if available locally)
+    for repo in build_deps:
+        src_deps = []
+        for dep in build_deps[repo]:
+            if dep in src_pkgs:
+                src_deps.append(src_pkgs[dep])
+        build_deps[repo] = src_deps
 
-def graph(dirs: List[Path], linear=True):
-    """Prints linear build order. If dpkg-checkbuilddeps is not available, a container
-    started with dbp.docker_run must be already running.
-    """
-    valids = []
-    for p in dirs:
-        if Path(p / "debian/control").is_file():
-            valids.append(p)
-        else:
-            L.info("Directory {} is missing a debian/control file".format(p.stem))
-
-    deps = {v.stem: [] for v in valids}
-
-    # time to collect all unmet build dependencies in parallel
-    commands = {}
-    for k, v in deps.items():
-        if shutil.which("dpkg-checkbuilddeps") is None:
-            commands[k] = docker_exec_command(
-                ["dpkg-checkbuilddeps", "{}/debian/control".format(k)]
-            )
-        else:
-            commands[k] = ["dpkg-checkbuilddeps", "{}/debian/control".format(k)]
-
-    processes = {
-        repo: Popen(cmd, stdout=PIPE, stderr=STDOUT, universal_newlines=True)
-        for repo, cmd in commands.items()
-    }
-
-    deps_map = get_source_map(valids)
-
-    for repo, proc in processes.items():
-        proc.wait()
-        # process returned build dependencies
-        if proc.returncode == 1:
-            for line in proc.stdout:
-                versioned_deps = line.replace(
-                    "dpkg-checkbuilddeps: error: Unmet build dependencies:", ""
-                ).strip()
-                build_deps = re.sub(r" \([^\)]*\)", "", versioned_deps).split(" ")
-                L.debug("Checking {} build deps {}".format(repo, build_deps))
-                for build_dep in build_deps:
-                    if build_dep in deps_map:
-                        deps[repo].append(deps_map[build_dep])
-
+    # Create graph from build deps
     graph = nx.DiGraph()
-    for k, v in deps.items():
+    for k, v in build_deps.items():
         graph.add_node(k)
         graph.add_nodes_from(v)
         for dep in v:
             graph.add_edge(k, dep)
 
-    if linear:
-        print(" ".join(nx.dfs_postorder_nodes(graph)))
-    else:
-        nx.nx_pydot.write_dot(graph, sys.stdout)
+    return graph
 
 
 def main() -> int:
@@ -114,9 +89,6 @@ def main() -> int:
     )
     parser.add_argument(
         "--verbose", "-v", help="-v for info, -vv for debug", action="count", default=0
-    )
-    parser.add_argument(
-        "--rm", action="store_true", help="remove container when finished"
     )
     parser.add_argument(
         "directories", type=Path, nargs="*", help="directories to graph"
@@ -151,24 +123,16 @@ def main() -> int:
     if len(args.directories) == 0:
         args.directories = [p for p in Path.cwd().iterdir() if p.is_dir()]
 
-    if shutil.which("dpkg-checkbuilddeps") is None:
-        # Using Docker, time to start container
-        if not dbp.container_exists():
-            rc = dbp.docker_run("opxhub/gbp", "stretch", "", dev=False)
-            if rc != 0:
-                L.error("Could not run container")
-                return rc
-
-        if not dbp.container_running("stretch"):
-            rc = dbp.docker_start("stretch")
-            if rc != 0:
-                L.error("Could not start stopped container")
-                return rc
-
-    graph(args.directories, args.linear)
-
-    if args.rm:
-        dbp.remove_container()
+    # Get graph and print
+    g = graph(args.directories)
+    if args.linear:
+        # Put all isolated nodes first in list
+        isolates = list(nx.isolates(g))
+        g.remove_nodes_from(isolates)
+        build_order = list(nx.dfs_postorder_nodes(g))
+        print(" ".join(isolates + build_order))
+    else:
+        nx.nx_pydot.write_dot(g, sys.stdout)
 
     return 0
 
